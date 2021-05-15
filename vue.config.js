f7947259a3697eeb27024122b37304d5a65a5b48
Karen-116 <962388544@qq.com>
2021-05-15 21:10:07 +0800

    初始化项目

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    outputDir: 'dist', //build输出目录
+    assetsDir: 'assets', //静态资源目录（js, css, img）
+    lintOnSave: false, //是否开启eslint
+}
+
+module.exports = {
+    configurewebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views'
+            }
+        }
+    }
+}
